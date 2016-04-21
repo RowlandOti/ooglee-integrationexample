51ce403571268b9c5fc238dce5b479d7d3feb10d
RowlandOti <rowlandotienoo@gmail.com>
2016-04-21 20:43:19 +0300

    refactor: undo npm dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,6 @@
 var elixir = require('laravel-elixir');
 var gulp = require("gulp");
 var bower = require("gulp-bower");
-// FTP
-var ftp = require('vinyl-ftp');
-var gutil = require('gulp-util');
-var minimist = require('minimist');
-var args = minimist(process.argv.slice(2));
 
-gulp.task('deploy', function() {
-  var remotePath = '/public_html/skyllabler.com/test';
-  var conn = ftp.create({
-    host: 'ftp.skyllabler.com',
-    user: args.user,
-    password: args.password,
-    log: gutil.log
-  });
-
-  gulp.src(['*', '*'])
-    .pipe(conn.newer(remotePath))
-    .pipe(conn.dest(remotePath));
-});
 
 
