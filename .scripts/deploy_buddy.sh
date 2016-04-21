e61431b61945ab0d12adeb72885a927d7920b1fa
RowlandOti <rowlandotienoo@gmail.com>
2016-04-22 00:37:38 +0300

    chore: change file extensions

--- .scripts/deploy_buddy.sh
+++ .scripts/deploy_buddy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #Deploy to git
 eval "$(ssh-agent -s)" #start the ssh agent
-chmod 600 .travis/deploy_key.pem # this key should have push access
-ssh-add .travis/deploy_key.pem
+chmod 600 .keys/deploy_key.pem # this key should have push access
+ssh-add .keys/deploy_key.pem
 git config --global user.email "rowlandotienoo@gmail.com"
 git config --global user.name "Travis-Bot-RowlandOti"
 git init
