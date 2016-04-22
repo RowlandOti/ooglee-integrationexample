3b571eff90560297f5d4d3b3aa8632612dba5624
RowlandOti <rowlandotienoo@gmail.com>
2016-04-22 11:25:21 +0300

    add: more options

--- .scripts/deploy_buddy.sh
+++ .scripts/deploy_buddy.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 #Deploy to git
+
+REPO_PATH=git@github.com:RowlandOti/deploy-zone.git
+COMMIT_MESS="deploy: automated deploy to server"
+COMMIT_USER="Travis-Bot-RowlandOti"
+COMMIT_EMAIL="travis-bot@rowlandoti.com"
+CHANGESET=$(git rev-parse --verify HEAD)
+
 eval "$(ssh-agent -s)" #start the ssh agent
 #chmod 600 .keys/deploy_key.pem # this key should have push access
 #ssh-add .keys/deploy_key.pem
@@ -9,10 +16,11 @@ mv .keys/authorised_keys ~/.ssh/authorised_keys
 chmod 600 ~/.ssh/id_rsa # this key should have push access
 chmod 600 ~/.ssh/authorized_keys
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-git config --global user.email "travis-bot@rowlandoti.com"
-git config --global user.name "Travis-Bot-RowlandOti"
+echo -e "Host github.com\n\tPasswordAuthentication no\n" >> ~/.ssh/config
+git config --global user.email "${COMMIT_EMAIL}"
+git config --global user.name "${COMMIT_USER}"
 git init
 git add .
-git commit -am "deploy: automated deploy to server"
-git remote add deploy git@github.com:RowlandOti/deploy-zone.git
+git commit -am "${COMMIT_MESS} with changeset ${CHANGESET}"
+git remote add deploy ${REPO_PATH}
 git push deploy
