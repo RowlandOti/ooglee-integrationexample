775e248865c5ff5bf0ab3ea2dc699190ef5e96cd
RowlandOti <rowlandotienoo@gmail.com>
2016-04-22 10:51:43 +0300

    fix: change perms

--- .scripts/deploy_buddy.sh
+++ .scripts/deploy_buddy.sh
@@ -7,6 +7,7 @@ mkdir -p ~/.ssh
 mv .keys/deploy_key.pem ~/.ssh/id_rsa
 mv .keys/authorised_keys ~/.ssh/authorised_keys
 chmod 600 ~/.ssh/id_rsa # this key should have push access
+chmod 600 ~/.ssh/authorized_keys
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 git config --global user.email "travis-bot@rowlandoti.com"
 git config --global user.name "Travis-Bot-RowlandOti"
