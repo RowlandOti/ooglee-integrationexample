b96a8a4d16cdac8a235e879e79c0da1cb8b7e219
RowlandOti <rowlandotienoo@gmail.com>
2016-04-22 11:43:55 +0300

    rectify: order

--- .scripts/deploy_buddy.sh
+++ .scripts/deploy_buddy.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 #Deploy to git
 
-REPO_PATH=git@github.com:RowlandOti/deploy-zone.git
-COMMIT_MESS="deploy: automated deploy to server"
-COMMIT_USER="Travis-Bot-RowlandOti"
-COMMIT_EMAIL="travis-bot@rowlandoti.com"
-CHANGESET=$(git rev-parse --verify HEAD)
+export REPO_PATH=git@github.com:RowlandOti/deploy-zone.git
+export COMMIT_MESS="deploy: automated deploy to server"
+export COMMIT_USER="Travis-Bot-RowlandOti"
+export COMMIT_EMAIL="travis-bot@rowlandoti.com"
+# Extract the commit message
+export TRAVIS_COMMIT_MSG="$(git log --format=%B --no-merges -n 1)"
+export CHANGESET="$(git rev-parse --verify HEAD)"
 
 eval "$(ssh-agent -s)" #start the ssh agent
 #chmod 600 .keys/deploy_key.pem # this key should have push access
@@ -17,6 +19,7 @@ chmod 600 ~/.ssh/id_rsa # this key should have push access
 chmod 600 ~/.ssh/authorized_keys
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 echo -e "Host github.com\n\tPasswordAuthentication no\n" >> ~/.ssh/config
+git config --global push.default matching
 git config --global user.email "${COMMIT_EMAIL}"
 git config --global user.name "${COMMIT_USER}"
 git init
