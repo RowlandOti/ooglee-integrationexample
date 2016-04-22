53be8df977b3a4c7ad50049a58866a20e29695bc
RowlandOti <rowlandotienoo@gmail.com>
2016-04-22 10:04:52 +0300

    try: remove error

--- .scripts/deploy_buddy.sh
+++ .scripts/deploy_buddy.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 #Deploy to git
 eval "$(ssh-agent -s)" #start the ssh agent
-chmod 600 .keys/deploy_key.pem # this key should have push access
-ssh-add .keys/deploy_key.pem
+#chmod 600 .keys/deploy_key.pem # this key should have push access
+#ssh-add .keys/deploy_key.pem
+mkdir -p ~/.ssh
+mv .keys/deploy_key.pem ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa # this key should have push access
+echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 git config --global user.email "travis-bot@rowlandoti.com"
 git config --global user.name "Travis-Bot-RowlandOti"
 git init
