c6266d546f048deee93772f809017f4469c3d2a8
RowlandOti <rowlandotienoo@gmail.com>
2016-04-22 08:25:06 +0300

    fix: track pem file

--- .scripts/deploy_buddy.sh
+++ .scripts/deploy_buddy.sh
@@ -3,7 +3,7 @@
 eval "$(ssh-agent -s)" #start the ssh agent
 chmod 600 deploy_key.pem # this key should have push access
 ssh-add deploy_key.pem
-git config --global user.email "rowlandotienoo@gmail.com"
+git config --global user.email "travis-bot@rowlandoti.com"
 git config --global user.name "Travis-Bot-RowlandOti"
 git init
 git add .
