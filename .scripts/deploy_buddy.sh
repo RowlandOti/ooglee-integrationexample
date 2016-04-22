43b92df2c014e3a369ec1ec61d4b50b9843dc45c
RowlandOti <rowlandotienoo@gmail.com>
2016-04-22 11:58:30 +0300

    fix: rename authorised keys

--- .scripts/deploy_buddy.sh
+++ .scripts/deploy_buddy.sh
@@ -10,15 +10,16 @@ export TRAVIS_COMMIT_MSG="$(git log --format=%B --no-merges -n 1)"
 export CHANGESET="$(git rev-parse --verify HEAD)"
 
 eval "$(ssh-agent -s)" #start the ssh agent
-#chmod 600 .keys/deploy_key.pem # this key should have push access
-#ssh-add .keys/deploy_key.pem
+
 mkdir -p ~/.ssh
 mv .keys/deploy_key.pem ~/.ssh/id_rsa
-mv .keys/authorised_keys ~/.ssh/authorised_keys
+mv .keys/authorized_keys ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/id_rsa # this key should have push access
 chmod 600 ~/.ssh/authorized_keys
+
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 echo -e "Host github.com\n\tPasswordAuthentication no\n" >> ~/.ssh/config
+
 git config --global push.default matching
 git config --global user.email "${COMMIT_EMAIL}"
 git config --global user.name "${COMMIT_USER}"
