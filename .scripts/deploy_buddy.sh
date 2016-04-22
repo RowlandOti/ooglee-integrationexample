b92629310f6b84cd522389084ba768ec9b807e40
RowlandOti <rowlandotienoo@gmail.com>
2016-04-22 12:54:07 +0300

    try: refactor

--- .scripts/deploy_buddy.sh
+++ .scripts/deploy_buddy.sh
@@ -9,8 +9,6 @@ export COMMIT_EMAIL="travis-bot@rowlandoti.com"
 export TRAVIS_COMMIT_MSG="$(git log --format=%B --no-merges -n 1)"
 export CHANGESET="$(git rev-parse --verify HEAD)"
 
-eval "$(ssh-agent -s)" #start the ssh agent
-
 mkdir -p ~/.ssh
 mv .keys/deploy_key.pem ~/.ssh/id_rsa
 mv .keys/authorized_keys ~/.ssh/authorized_keys
@@ -18,13 +16,13 @@ chmod 600 ~/.ssh/id_rsa # this key should have push access
 chmod 600 ~/.ssh/authorized_keys
 
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-echo -e "Host github.com\n\tPasswordAuthentication no\n" >> ~/.ssh/config
+eval "$(ssh-agent -s)" #start the ssh agent
 
 git config --global push.default matching
 git config --global user.email "${COMMIT_EMAIL}"
 git config --global user.name "${COMMIT_USER}"
 git init
-git add .
+git add --all .
 git commit -am "${COMMIT_MESS} with changeset ${CHANGESET}"
 git remote add deploy ${REPO_PATH}
 git push deploy
