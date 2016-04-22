8c6cbc9c8961e9af3cb4b5fcb7e684b1a1a1dce2
RowlandOti <rowlandotienoo@gmail.com>
2016-04-22 13:20:42 +0300

    refactor: add ssh script

---
+++ .scripts/configure_ssh.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#Configure SSH
+
+export KEYS_DIRECTORY=.keys
+export SSH_DIRECTORY=~/.ssh
+
+if [ -d "${KEYS_DIRECTORY}" ]; then
+   touch ${KEYS_DIRECTORY}/deploy_key.pem
+else
+   echo "Error: ${KEYS_DIRECTORY} not found ....Creating.*.*.*."
+   mkdir ${KEYS_DIRECTORY}
+   touch ${KEYS_DIRECTORY}/deploy_key.pem
+fi
+
+openssl aes-256-cbc -nopad -K $encrypted_fd30640532d7_key -iv $encrypted_fd30640532d7_iv -in ${KEYS_DIRECTORY}/deploy_key.pem.enc -out ${KEYS_DIRECTORY}/deploy_key.pem -d
+
+if [ -d "${SSH_DIRECTORY}" ]; then
+   mv .keys/deploy_key.pem ${SSH_DIRECTORY}/id_rsa
+   mv .keys/authorized_keys ${SSH_DIRECTORY}/authorized_keys
+else
+   echo "Error: ${SSH_DIRECTORY} not found ....Creating.*.*.*."
+   mkdir -p ${SSH_DIRECTORY}
+   mv .keys/deploy_key.pem ${SSH_DIRECTORY}/id_rsa
+   mv .keys/authorized_keys ${SSH_DIRECTORY}/authorized_keys
+fi
+
+chmod 600 ${SSH_DIRECTORY}/id_rsa # this key should have push access
+chmod 600 ${SSH_DIRECTORY}/authorized_keys
+
+echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ${SSH_DIRECTORY}/config
+eval "$(ssh-agent -s)" #start the ssh agent
+
+ls -al
--- .scripts/deploy_buddy.sh
+++ .scripts/deploy_buddy.sh
@@ -2,27 +2,18 @@
 #Deploy to git
 
 export REPO_PATH=git@github.com:RowlandOti/deploy-zone.git
-export COMMIT_MESS="deploy: automated deploy to server"
+export COMMIT_MSG="deploy: Automated deploy to server"
 export COMMIT_USER="Travis-Bot-RowlandOti"
 export COMMIT_EMAIL="travis-bot@rowlandoti.com"
 # Extract the commit message
 export TRAVIS_COMMIT_MSG="$(git log --format=%B --no-merges -n 1)"
 export CHANGESET="$(git rev-parse --verify HEAD)"
 
-mkdir -p ~/.ssh
-mv .keys/deploy_key.pem ~/.ssh/id_rsa
-mv .keys/authorized_keys ~/.ssh/authorized_keys
-chmod 600 ~/.ssh/id_rsa # this key should have push access
-chmod 600 ~/.ssh/authorized_keys
-
-echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-eval "$(ssh-agent -s)" #start the ssh agent
-
 git config --global push.default matching
 git config --global user.email "${COMMIT_EMAIL}"
 git config --global user.name "${COMMIT_USER}"
 git init
 git add --all .
-git commit -am "${COMMIT_MESS} with changeset ${CHANGESET}"
+git commit -am "${COMMIT_MSG} - ${TRAVIS_COMMIT_MSG} with changeset ${CHANGESET}"
 git remote add deploy ${REPO_PATH}
 git push deploy
